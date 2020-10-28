7d6628df943bb40fea3a145c48a94a82205d3ca4
Cancio <filipe.cancio@gmail.com>
2020-10-28 10:46:42 -0300

    feat: update api content

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const cors = require('cors');
+const home = require('./src/util/home.json');
+const contacts = require('./src/util/contacts.json');
+const transactions = require('./src/util/transactions.json');
+const query = require('./src/util/transactionsQuery.json');
+
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).send('Deu Certo').end();
+app.use(express.json());
+app.use(cors());
+
+app.get('/home',(request,response)=>{
+    return response.json(home);
+});
+
+app.get('/contacts',(request,response)=>{
+    return response.json(contacts);
+});
+
+app.get('/transactions',(request,response)=>{
+    return response.json(transactions);
+});
+
+app.get('/query',(request,response)=>{
+    return response.json(query);
 });
 
 
